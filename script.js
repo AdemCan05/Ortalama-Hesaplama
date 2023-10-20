1fbd05926be0a36bc8d015a8bfdddbc50c6dc0e6
Adem Can Demirci <140834555+AdemCan05@users.noreply.github.com>
2023-10-20 22:03:39 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-# Ortalama Hesaplama
-
-
-## CS50
->This was my final project for conclude the CS50 Introduction to Computer Sciense course
-
-> CSS, HTML, JS, web devolopment, CS50
-
-I first used html to create my page, then I used javascript to make my site function, and finally I used css to adjust its appearance.
-
-## The purpose of the web page I created
-The purpose of my site is to indicate that the school where I study is successful or unsuccessful according to the scores we receive as a result of the exams as a letter grade and as a result of the exam.
-
-## How do I calculate the average
-I added a js code that shows whether the grades I use to calculate the average are valid or not, should be a number between 0 and 100, and how much the midterm and final grades affect the average.
-
-```
-javascript
 function hesapla() {
     var vize = parseFloat(document.getElementsByName("vize")[0].value);
     var final = parseFloat(document.getElementsByName("finalbüt")[0].value);
@@ -32,14 +14,9 @@ function hesapla() {
     var ortalama = (vize * 0.4) + (final * 0.6);
 
     var sonuc = document.getElementById("sonuc");
-```
-### Calculation of letter grade and success status according to the average
 
-Here we are using an if else if loop via javascript to get output according to the state of the average.
-
-```
-javascript
-if (ortalama >= 90) {
+    // Ortalamaya göre notlar ve başarı durumu
+    if (ortalama >= 90) {
         sonuc.innerHTML = "Ortalama: AA<br>Notunuz: Başarılı";
     } else if (ortalama >= 80) {
         sonuc.innerHTML = "Ortalama: BA<br>Notunuz: Başarılı";
@@ -60,9 +37,3 @@ document.getElementById("grade-form").addEventListener("submit", function (e) {
     e.preventDefault(); // Form gönderimini engelle
     hesapla(); // Hesapla fonksiyonunu çağır
 });
-```
-## Demonstration on youtube
-https://youtu.be/WotfDPjF58o
-
-## Networks
-[LinkedIn Adem Can Demirci](https://www.linkedin.com/in/adem-can-demirci-100acd100/)
